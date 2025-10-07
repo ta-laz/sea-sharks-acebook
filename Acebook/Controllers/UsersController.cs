54c5f79053cb6774c6466dcc1a12088712c7ce9a
Karan Kullar <jaskaran.kullar.13@alumni.ucl.ac.uk>
2025-10-07 15:55:19 +0100

    added password logic for error

--- Acebook/Controllers/UsersController.cs
+++ Acebook/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using acebook.Models;
+using System.Security.Cryptography;
+
 
 namespace acebook.Controllers;
 
@@ -22,16 +24,34 @@ public class UsersController : Controller
 
     [Route("/users")]
     [HttpPost]
-    public RedirectResult Create(User user) {
-      AcebookDbContext dbContext = new AcebookDbContext();
-      dbContext.Users.Add(user);
-      dbContext.SaveChanges();
-      return new RedirectResult("/signin");
+    public IActionResult Create(User user, string confirmPassword)
+    {
+
+        if (user.Password != confirmPassword)
+        {
+            ViewBag.Error = "Passwords do not match.";
+            return View("New",user); // or whatever your signup view is called
+        }
+        AcebookDbContext dbContext = new AcebookDbContext();
+        string hashed = HashPassword(user.Password);
+        user.Password = hashed;
+        dbContext.Users.Add(user);
+        dbContext.SaveChanges();
+        return new RedirectResult("/signin");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+    private static string HashPassword(string password)
+    {
+        // Minimal example; prefer ASP.NET Core Identity's PasswordHasher<TUser>
+        using var sha256 = SHA256.Create();
+        var bytes = System.Text.Encoding.UTF8.GetBytes(password);
+        var hash = sha256.ComputeHash(bytes);
+        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
     }
 }
