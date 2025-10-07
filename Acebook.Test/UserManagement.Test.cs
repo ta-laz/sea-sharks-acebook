3c1aa35970fdd9946bf5abac2a3236ff2fb42bb1
Karan Kullar <jaskaran.kullar.13@alumni.ucl.ac.uk>
2025-10-07 13:43:46 +0100

    sign in test passes

--- Acebook.Test/UserManagement.Test.cs
+++ Acebook.Test/UserManagement.Test.cs
@@ -52,24 +52,16 @@ namespace Acebook.Tests
 
     [Test]
     public void SignIn_ValidCredentials_RedirectToPosts() {
-      // driver.Navigate().GoToUrl("http://127.0.0.1:5287");
-      IWebElement signUpButton = driver.FindElement(By.Id("signup"));
-      // signUpButton.Click();
-      IWebElement nameField = driver.FindElement(By.Id("name"));
-      // nameField.SendKeys("francine");
-      IWebElement emailField = driver.FindElement(By.Id("email"));
-      // emailField.SendKeys("finn.white@sharkmail.ocean");
-      IWebElement passwordField = driver.FindElement(By.Id("password"));
-      // passwordField.SendKeys("password123");
-      IWebElement submitButton = driver.FindElement(By.Id("submit"));
-      // submitButton.Click();
 
       driver.Navigate().GoToUrl("http://127.0.0.1:5287/signin");
-      emailField = driver.FindElement(By.Id("email"));
+      IWebElement emailField = driver.FindElement(By.Id("email"));
+      IWebElement passwordField = driver.FindElement(By.Id("password"));
+      IWebElement submitButton = driver.FindElement(By.Id("submit"));
+      // emailField = driver.FindElement(By.Id("email"));
       emailField.SendKeys("finn.white@sharkmail.ocean");
-      passwordField = driver.FindElement(By.Id("password"));
-      passwordField.SendKeys("password123");
-      submitButton = driver.FindElement(By.Id("submit"));
+      // passwordField = driver.FindElement(By.Id("password"));
+      passwordField.SendKeys("da2cb7f780b225403e5487ce7d40feaa0283f663ce05c7882df100110e8aae86");
+      // submitButton = driver.FindElement(By.Id("submit"));
       submitButton.Click();
       string currentUrl = driver.Url;
       Assert.That(currentUrl, Is.EqualTo("http://127.0.0.1:5287/posts"));
