b928d37c5dc03f9f72463db637c63e2722cbdef9
Tom Wight <tomwight233@gmail.com>
2025-10-08 17:56:17 +0100

    Update SignInViewModel.cs
    Removed commented out lines

--- Acebook/ViewModels/SignInViewModel.cs
+++ Acebook/ViewModels/SignInViewModel.cs
@@ -9,8 +9,6 @@ public class SignInViewModel : IValidatableObject
 
     [Required(ErrorMessage = "Password is required")]
     [DataType(DataType.Password)]
-    //[RegularExpression(@"^(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,}$",
-        //ErrorMessage = "Password must be ≥ 8 chars and include an uppercase letter and a special character.")]
     public string Password { get; set; } = "";
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
